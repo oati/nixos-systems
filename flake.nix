b272aa414512d1a0697fc566fe0ed44511381ba5
Erin <79354991+oati@users.noreply.github.com>
2026-03-11 21:29:19 -0500

    remove nix config from flake

--- flake.nix
+++ flake.nix
@@ -31,18 +31,6 @@
     };
   };
 
-  nixConfig = {
-    extra-substituters = [
-      "https://nix-community.cachix.org"
-      "https://install.determinate.systems"
-    ];
-
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
-    ];
-  };
-
   outputs = flakes: {
     nixosConfigurations = builtins.mapAttrs (
       name: modules:
