967060779f561683bb3688cb2cbde4909473a1cd
Erin <79354991+oati@users.noreply.github.com>
2026-03-12 22:50:45 -0500

    build opentabletdriver from source for scroll wheel support

--- modules/hardware/peripherals/drawing-tablet/default.nix
+++ modules/hardware/peripherals/drawing-tablet/default.nix
@@ -7,4 +7,13 @@
   intransience.datastores.home.users.${user}.dirs = [
     ".config/OpenTabletDriver"
   ];
+
+  # remove when opentabletdriver > v0.6.6.2
+  nixpkgs.overlays = [
+    (final: prev: {
+      # nix build .#nixosConfigurations.erin-pc.pkgs.opentabletdriver.fetch-deps
+      # ./result modules/hardware/peripherals/drawing-tablet/packages/deps.json
+      opentabletdriver = final.callPackage ./packages/opentabletdriver.nix {};
+    })
+  ];
 }
