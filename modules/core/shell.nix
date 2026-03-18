74e8e8f40bc97ac2927893b9bbb8cd0c665322be
Erin <79354991+oati@users.noreply.github.com>
2026-03-18 14:34:49 -0500

    add fswatch command

--- modules/core/shell.nix
+++ modules/core/shell.nix
@@ -45,6 +45,7 @@
               /nix/var/nix/profiles/system/bin/switch-to-configuration test
             }
           }
+
           def "nixos-generations list" []: nothing -> table {
             nixos-rebuild list-generations --json | from json
               | update date {into datetime}
@@ -52,15 +53,29 @@
               | move current --after generation
               | move revision --after date
           }
+
           def "nixos-generations switch" [generation: int] {
             run0 nix-env -p /nix/var/nix/profiles/system --switch-generation $generation
           }
+
           # besides a list of generation numbers to delete, this command can take
           # - +N - delete generations older than the last N generations starting from current
           # - Nd - delete all but the most recent generation older than N days
           # - old - delete all generations except current
           def "nixos-generations delete" [...generations] {
             run0 nix-env -p /nix/var/nix/profiles/system --delete-generations ...$generations
+          }
+
+          # watch the filesystem of a directory for changes
+          def fswatch [dir] {
+            (
+              run0 fsnotifywait ($dir | path expand)
+              --filesystem
+              --monitor
+              --quiet
+              --event create,delete,modify,move
+              --format "%e\t%w%f"
+            )
           }
         '';
     };
