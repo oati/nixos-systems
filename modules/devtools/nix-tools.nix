2307c1330e15c47b1a95ea6179cd0efb9d858a32
Erin <79354991+oati@users.noreply.github.com>
2026-03-18 17:40:14 -0500

    fix completions for nix-your-shell enable nix-ouptut-monitor for nix-your-shell

--- modules/devtools/nix-tools.nix
+++ modules/devtools/nix-tools.nix
@@ -1,4 +1,8 @@
-{user, ...}: {
+{
+  user,
+  pkgs,
+  ...
+}: {
   home-manager.users.${user} = {
     programs.nix-index = {
       enable = true;
@@ -6,6 +10,18 @@
 
     programs.nix-your-shell = {
       enable = true;
+      nix-output-monitor.enable = true;
+
+      # remove when nix-your-shell > 1.4.9
+      package = pkgs.nix-your-shell.overrideAttrs (prev: {
+        version = "git";
+        src = pkgs.fetchFromGitHub {
+          owner = "oati";
+          repo = "nix-your-shell";
+          rev = "f88fe1d00cfeb875d5452de5bf670ded63a2da1f";
+          hash = "sha256-YBnfByywQY/oR6GahGnao1TIWLHxTZAsnMAuh8RmCw0=";
+        };
+      });
     };
 
     programs.direnv = {
