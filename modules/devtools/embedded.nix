778d3dafcea566e0e3eee2070b2ca302842ad064
Erin <79354991+oati@users.noreply.github.com>
2026-03-31 02:04:14 -0500

    fix udev rules for embedded development

--- modules/devtools/embedded.nix
+++ modules/devtools/embedded.nix
@@ -1,50 +1,59 @@
-{lib, ...}: {
-  services.udev = {
-    extraRules = let
-      ids = [
-        # raspberry pi
-        # {vendor = "2e8a";}
+{
+  lib,
+  pkgs,
+  ...
+}: {
+  # see https://github.com/NixOS/nixpkgs/issues/308681
+  services.udev.packages = [
+    (pkgs.writeTextFile {
+      name = "usb-udev-rules";
+      destination = "/etc/udev/rules.d/70-usb.rules";
+      text = let
+        ids = [
+          # raspberry pi
+          # {vendor = "2e8a";}
 
-        # adafruit bootloader
-        # {vendor = "2fe3";}
+          # adafruit bootloader
+          # {vendor = "2fe3";}
 
-        # nordic semiconductor
-        # {vendor = "1915";}
+          # nordic semiconductor
+          # {vendor = "1915";}
 
-        # atmega32u4
-        {
-          vendor = "03eb";
-          product = "2ff4";
-        }
-      ];
-    in
-      lib.concatMapStringsSep "\n" (
-        {
-          vendor,
-          product ? null,
-          products ?
-            if product != null
-            then [product]
-            else null,
-        }:
-          if products != null
-          then
-            lib.concatMapStringsSep "\n"
-            (product:
+          # atmega32u4
+          {
+            vendor = "03eb";
+            product = "2ff4";
+          }
+        ];
+      in
+        lib.concatMapStringsSep "\n" (
+          {
+            vendor,
+            product ? null,
+            products ?
+              if product != null
+              then [product]
+              else null,
+          }:
+            if products != null
+            then
+              lib.concatMapStringsSep "\n"
+              (product:
+                lib.concatStringsSep ", " [
+                  ''SUBSYSTEMS=="usb"''
+                  ''ATTRS{idVendor}=="${vendor}"''
+                  ''ATTRS{idProduct}=="${product}"''
+                  ''TAG+="uaccess"''
+                ])
+              products
+            else
               lib.concatStringsSep ", " [
                 ''SUBSYSTEMS=="usb"''
                 ''ATTRS{idVendor}=="${vendor}"''
-                ''ATTRS{idProduct}=="${product}"''
                 ''TAG+="uaccess"''
-              ])
-            products
-          else
-            lib.concatStringsSep ", " [
-              ''SUBSYSTEMS=="usb"''
-              ''ATTRS{idVendor}=="${vendor}"''
-              ''TAG+="uaccess"''
-            ]
-      )
-      ids;
-  };
+              ]
+        )
+        ids;
+    })
+  ];
 }
