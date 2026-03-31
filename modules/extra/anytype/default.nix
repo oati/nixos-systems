496b0d6b4e422c4b880d506e3f4c6bad211f5e9f
Erin <79354991+oati@users.noreply.github.com>
2026-03-31 03:33:53 -0500

    fix anytype desktop entry

--- modules/extra/anytype/default.nix
+++ modules/extra/anytype/default.nix
@@ -7,7 +7,15 @@
     ./appearance.nix
   ];
 
-  environment.systemPackages = [pkgs.anytype];
+  home-manager.users.${user} = {
+    home.packages = [pkgs.anytype];
+
+    # prevent anytype from automatically creating bad desktop entry
+    xdg.dataFile."applications/anytype.desktop" = {
+      source = "${pkgs.anytype}/share/applications/anytype.desktop";
+      force = true;
+    };
+  };
 
   intransience.datastores.home.users.${user}.dirs = [
     ".config/anytype"
