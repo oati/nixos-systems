602af73bdcb57f91d860cee3eabf895df2ab7a74
Erin <79354991+oati@users.noreply.github.com>
2026-04-01 16:37:13 -0500

    configure pdf viewer

--- modules/role/workstation/pdf-viewer/default.nix
+++ modules/role/workstation/pdf-viewer/default.nix
@@ -3,15 +3,21 @@
     programs.sioyek = {
       enable = true;
 
+      # https://sioyek-documentation.readthedocs.io/en/latest/configuration.html
       config = {
+        should_launch_new_window = "1";
+        should_load_tutorial_when_no_other_file = "0";
+
+        # generate search index
+        super_fast_search = "1";
+        prerender_page_count = "8";
+
         custom_background_color = "0.0 0.0 0.0";
         custom_text_color = "1.0 1.0 1.0";
 
-        super_fast_search = "";
-        prerender_page_count = "8";
-
         startup_commands = [
           "toggle_custom_color"
+          "toggle_visual_scroll"
         ];
       };
     };
