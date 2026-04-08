e7a76d1621a0a33d7e2e691e2a5aac18b869e686
Erin <79354991+oati@users.noreply.github.com>
2026-04-08 12:25:59 -0500

    configure nushell table formatting

--- modules/core/shell/default.nix
+++ modules/core/shell/default.nix
@@ -17,9 +17,22 @@
     programs.nushell = {
       enable = true;
 
+      # config nu --doc
       settings = {
         show_banner = false;
         history.file_format = "sqlite";
+
+        # default "if (term size).columns >= 100 { table -e } else { table }"
+        hooks.display_output = "table";
+
+        # number of rows required to show footer
+        footer_mode = 25;
+
+        # truncate last column instead of wrapping
+        table.trim = {
+          methodology = "truncating";
+          truncating_suffix = "...";
+        };
       };
     };
 
