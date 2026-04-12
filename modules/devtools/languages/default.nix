f527d81ce3a30ceec5ea5d02821d0462648c55e3
Erin <79354991+oati@users.noreply.github.com>
2026-04-12 14:02:11 -0500

    configure uv for python

--- modules/devtools/languages/default.nix
+++ modules/devtools/languages/default.nix
@@ -6,6 +6,7 @@
   imports = [
     ./nix.nix
     ./rust.nix
+    ./python.nix
   ];
 
   home-manager.users.${user} = {
---
+++ modules/devtools/languages/python.nix
@@ -0,0 +1,18 @@
+{user, ...}: {
+  home-manager.users.${user} = {
+    programs.uv = {
+      enable = true;
+
+      settings = {
+        # do not manage python installations
+        python-downloads = "never";
+        python-preference = "only-system";
+      };
+    };
+  };
+
+  # see https://docs.astral.sh/uv/reference/storage
+  intransience.datastores.cache.users.${user}.dirs = [
+    ".cache/uv"
+  ];
+}
