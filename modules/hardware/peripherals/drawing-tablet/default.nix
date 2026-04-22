f133420832c82b9f95c850b6eeaf128318b647d1
Erin <79354991+oati@users.noreply.github.com>
2026-04-22 18:29:19 -0500

    refactor drawing tablet packages

--- modules/hardware/peripherals/drawing-tablet/default.nix
+++ modules/hardware/peripherals/drawing-tablet/default.nix
@@ -1,4 +1,8 @@
 {user, ...}: {
+  imports = [
+    ./packages
+  ];
+
   hardware.opentabletdriver.enable = true;
 
   hardware.uinput.enable = true;
@@ -6,14 +10,5 @@
 
   intransience.datastores.home.users.${user}.dirs = [
     ".config/OpenTabletDriver"
-  ];
-
-  # remove when opentabletdriver > v0.6.6.2
-  nixpkgs.overlays = [
-    (final: prev: {
-      # nix build .#nixosConfigurations.erin-pc.pkgs.opentabletdriver.fetch-deps
-      # ./result modules/hardware/peripherals/drawing-tablet/packages/deps.json
-      opentabletdriver = final.callPackage ./packages/opentabletdriver.nix {};
-    })
   ];
 }
