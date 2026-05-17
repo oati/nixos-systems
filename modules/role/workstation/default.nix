be5c4cd6acae341e0bfdd9377ee20ebfb92b9faa
Erin <79354991+oati@users.noreply.github.com>
2026-05-17 13:23:33 -0500

    add geary mail client

--- modules/role/workstation/default.nix
+++ modules/role/workstation/default.nix
@@ -14,6 +14,7 @@
     ./file-manager.nix
     ./media-player.nix
     ./pdf-viewer
+    ./email-client.nix
     ./browser
   ];
 
---
+++ modules/role/workstation/email-client.nix
@@ -0,0 +1,14 @@
+{
+  user,
+  pkgs,
+  ...
+}: {
+  home-manager.users.${user} = {
+    home.packages = [pkgs.geary];
+  };
+
+  intransience.datastores.home.users.${user}.dirs = [
+    ".config/geary"
+    ".local/share/geary"
+  ];
+}
