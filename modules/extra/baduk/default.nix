0e77a6a99eca9159d586eb402756a7be99a7e5f9
Erin <79354991+oati@users.noreply.github.com>
2026-05-29 19:51:59 -0500

    add configuration for sabaki

--- modules/extra/baduk/default.nix
+++ modules/extra/baduk/default.nix
@@ -8,10 +8,29 @@
     ./packages
   ];
 
-  home-manager.users.${user} = {
-    home.packages = [
-      pkgs.katago
-      pkgs.sabaki
-    ];
-  };
+  home-manager.users.${user} =
+    { lib, ... }:
+    {
+      home.packages = [
+        pkgs.katago
+        pkgs.sabaki
+      ];
+
+      home.activation = {
+        setSabakiSettings =
+          let
+            settings = { };
+          in
+          lib.hm.dag.entryAfter [ "writeBoundary" ]
+            # sh
+            ''
+              run mkdir -p $VERBOSE_ARG ~/.config/Sabaki
+              run cp $VERBOSE_ARG \
+                --remove-destination \
+                --no-preserve=mode \
+                ${pkgs.writers.writeJSON "sabaki-settings" settings} \
+                ~/.config/Sabaki/settings.json
+            '';
+      };
+    };
 }
