461f8b1cb61b9c668d8c88010f6cadc142835ba6
Erin <79354991+oati@users.noreply.github.com>
2026-06-16 17:15:12 -0500

    clean up kicad desktop entries

--- modules/extra/kicad/default.nix
+++ modules/extra/kicad/default.nix
@@ -1,6 +1,17 @@
 { user, pkgs, ... }:
 let
-  package = pkgs.kicad;
+  package = pkgs.symlinkJoin {
+    pname = "kicad-custom";
+    inherit (pkgs.kicad) version;
+    paths = [ pkgs.kicad ];
+
+    # clean up desktop entries
+    postBuild = ''
+      rm $out/share/applications
+      mkdir $out/share/applications
+      cp ${pkgs.kicad}/share/applications/org.kicad.kicad.desktop $out/share/applications
+    '';
+  };
 in
 {
   home-manager.users.${user} = {
