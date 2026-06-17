680ba2b7ba3cc84f7256e6f9eb304b9d86cfaec3
Erin <79354991+oati@users.noreply.github.com>
2026-06-16 23:06:42 -0500

    fix file picker crash for certain gtk3 applications issue was encountered with freecad

--- modules/role/workstation/xdg.nix
+++ modules/role/workstation/xdg.nix
@@ -1,4 +1,4 @@
-{ user, ... }:
+{ user, pkgs, ... }:
 {
   home-manager.users.${user} = {
     xdg = {
@@ -6,4 +6,10 @@
       mimeApps.enable = true;
     };
   };
+
+  # workaround for gtk3 file picker crash
+  # https://github.com/NixOS/nixpkgs/issues/149812
+  environment.sessionVariables.XDG_DATA_DIRS = [
+    "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}"
+  ];
 }
