8f699986d6996716969bc7d3c3b8f0f88bd2864d
Erin <79354991+oati@users.noreply.github.com>
2026-06-26 00:21:51 -0500

    remove redundant drawing tablet option

--- modules/hardware/peripherals/drawing-tablet/default.nix
+++ modules/hardware/peripherals/drawing-tablet/default.nix
@@ -3,7 +3,6 @@
   hardware.opentabletdriver.enable = true;
 
   hardware.uinput.enable = true;
-  boot.kernelModules = [ "uinput" ];
 
   intransience.datastores.home.users.${user}.dirs = [
     ".config/OpenTabletDriver"
