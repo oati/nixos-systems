d2f46ae049e4e343edadd746f62ea33063b81658
Erin <79354991+oati@users.noreply.github.com>
2026-06-26 16:49:03 -0500

    minor refactor nixpkgs overlay

--- modules/core/nixpkgs.nix
+++ modules/core/nixpkgs.nix
@@ -10,8 +10,8 @@
         in
         {
           inherit (packages flakes.determinate-nix) nix;
-          inherit (packages flakes.home-manager) home-manager;
           inherit (packages flakes.disko) disko;
+          inherit (packages flakes.home-manager) home-manager;
           inherit (packages flakes.delta-shell) delta-shell;
           inherit (packages flakes.iloader) iloader;
           nix-index = (packages flakes.nix-index-database).nix-index-with-db;
