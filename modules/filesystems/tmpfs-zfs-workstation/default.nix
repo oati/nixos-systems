41d486e27be6225426c9c8fa96a66f6c97208537
Erin <79354991+oati@users.noreply.github.com>
2026-07-12 04:30:53 -0500

    address zfs ram usage

--- modules/filesystems/tmpfs-zfs-workstation/default.nix
+++ modules/filesystems/tmpfs-zfs-workstation/default.nix
@@ -13,4 +13,8 @@
   intransience.datastores.system.etc.files = [
     "zfs/zpool.cache"
   ];
+
+  # workaround for https://github.com/openzfs/zfs/issues/10255
+  # amount of RAM that ZFS cache should leave free
+  boot.extraModprobeConfig = "options zfs zfs_arc_sys_free=${toString (4 * 1073741824)}"; # 4 GB
 }
