9e2d8a9b5f7a6b65d33c528856f503822e95f72a
Erin <79354991+oati@users.noreply.github.com>
2026-07-18 18:20:42 -0500

    make file manager desktop entry open a new window

--- modules/role/workstation/file-manager.nix
+++ modules/role/workstation/file-manager.nix
@@ -4,7 +4,21 @@
   ...
 }:
 let
-  package = pkgs.nautilus;
+  package = pkgs.symlinkJoin {
+    name = "nautilus-custom";
+    inherit (pkgs.nautilus) version meta;
+    paths = [ pkgs.nautilus ];
+
+    # make desktop entry open in new window
+    # remove DBusActivatable=true since it ignores Exec
+    postBuild = ''
+      pushd $out/share/applications
+      rm org.gnome.Nautilus.desktop
+      cp ${pkgs.nautilus}/share/applications/org.gnome.Nautilus.desktop .
+      sed -i '/^DBusActivatable=true$/d' org.gnome.Nautilus.desktop
+      popd
+    '';
+  };
 in
 {
   home-manager.users.${user} = {
